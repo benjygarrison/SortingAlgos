2ace3da9fc7df2a5357e82247a888a8adba18b79
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-01-06 22:56:29 -0800

    Added bubble sort

--- SortingAlgos/main.swift
+++ SortingAlgos/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ Sorting methods.
+ 
+ MARK: Top 3 to know: Bubble, Merge, Quick
+ 
+ Bubble sort = simplest. Compare items (in array, for example), and "bubble the largest value up by comparing it against every item after."
+ex: 5, 4, 3, 2, 1 -> Compare the 5 until it reaches end, keep passing with smaller numbers.
+ 
+ 
+ */
+
+
+//Bubble sort code:
+
 
