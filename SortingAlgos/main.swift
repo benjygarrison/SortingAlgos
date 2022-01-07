ac7723ce250db25964eba3551912336909bc5d3b
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-01-07 01:08:41 -0800

    updated bubble sort

--- SortingAlgos/main.swift
+++ SortingAlgos/main.swift
@@ -14,11 +14,32 @@ import Foundation
  
  Bubble sort = simplest. Compare items (in array, for example), and "bubble the largest value up by comparing it against every item after."
 ex: 5, 4, 3, 2, 1 -> Compare the 5 until it reaches end, keep passing with smaller numbers.
- 
+//MARK: Two sweep, internal for loop
  
  */
 
 
 //Bubble sort code:
 
+class BubbleSort {
+    func sort(_ array: [Int]) -> [Int] {
+        var arr = array
+        let n = arr.count
+        
+        for i in 0..<n-1 {
+            for j in 0..<n-i-1 {
+                if arr[j] > arr[j+1] {
+                    //swap
+                    let temp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+                }
+            }
+        }
+        return arr
+    }
+}
+
+let bubbleSort = BubbleSort()
+print("A sorted array: \(bubbleSort.sort([5, 4, 6, 2, 1]))")
 
